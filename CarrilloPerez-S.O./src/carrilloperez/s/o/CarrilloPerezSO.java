7215a14d5c98669958f8050b8d09a666e11a3a12
NeliCarrillo <ncx2005@gmail.com>
2025-02-05 00:02:57 -0400

    andiamo

--- CarrilloPerez-S.O./src/carrilloperez/s/o/CarrilloPerezSO.java
+++ CarrilloPerez-S.O./src/carrilloperez/s/o/CarrilloPerezSO.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package carrilloperez.s.o;
-
+import GUIs.Inicio;
 /**
  *
  * @author nelsoncarrillo
@@ -15,6 +15,10 @@ public class CarrilloPerezSO {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+        inicio.setResizable(false);
     }
     
 }
